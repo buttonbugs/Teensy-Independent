166d2234ef6519978764c95e0be0f56fa7c8283b
buttonbugs <miller.chen2006@outlook.com>
2026-01-19 10:41:17 -0700

    Add configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <Arduino.h>
 
+#include "config.h"
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWriteFast(LED_BUILTIN, HIGH);
-  delay(50);  // wait for a second
-
-  // turn the LED off by making the voltage LOW
-  digitalWriteFast(LED_BUILTIN, LOW);
-  delay(50);  // wait for a second
+    // turn the LED on (HIGH is the voltage level)
+    digitalWriteFast(LED_BUILTIN, HIGH);
+    delay(LED_TEST);  // wait for a second
+    
+    // turn the LED off by making the voltage LOW
+    digitalWriteFast(LED_BUILTIN, LOW);
+    delay(LED_TEST);  // wait for a second
 }
